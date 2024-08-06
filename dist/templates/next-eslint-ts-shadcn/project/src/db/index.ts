2dcb326e5577127eb4b5cf1c291f555a1aaf7fe4
Pablo Aller <pablusprogramador@gmail.com>
2024-08-06 11:41:15 +0200

    bump version

--- dist/templates/next-eslint-ts-shadcn/project/src/db/index.ts
+++ dist/templates/next-eslint-ts-shadcn/project/src/db/index.ts
@@ -1,9 +1,24 @@
 import { config } from "dotenv";
-import { drizzle } from "drizzle-orm/postgres-js";
+import { drizzle, type PostgresJsDatabase } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+
+import * as schema from "./schema";
 
 config({ path: ".env" });
 
 const client = postgres(process.env.DATABASE_URL ?? "");
 
-export const db = drizzle(client);
+declare global {
+  var db: PostgresJsDatabase<typeof schema> | undefined;
+}
+let db: PostgresJsDatabase<typeof schema>;
+
+if (process.env.NODE_ENV === "production") {
+  db = drizzle(client, { schema });
+} else {
+  if (!global.db) global.db = drizzle(client, { schema });
+
+  db = global.db;
+}
+
+export { db };
